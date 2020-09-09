1088eb7896708f23207e3cfdbb4e0c6cfd132c4d
luismvargasg <1610@holbertonschool.com>
2020-09-09 21:18:33 +0000

    adding endpoint that retrieves the number of objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,21 @@
 """File that routes the status on the object app_views"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage as st
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Method to display the welcome text"""
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Method that retrieves the number of each object by type"""
+    return jsonify(amenities=st.count('Amenity'),
+                   cities=st.count('City'),
+                   places=st.count('Place')
+                   reviews=st.count('Review'),
+                   states=st.count('State'),
+                   users=st.count('User'))
