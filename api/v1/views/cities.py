83de6abce3974387e5f09160faf388676a8705e1
acamilojuan <1600@holbertonschool.com>
2020-09-10 12:30:46 -0500

    cities.py updated for Task 8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -4,49 +4,50 @@ from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage as st
 from models.state import State
+from models.city import City
 
 
 @app_views.route('/states/<state_id>/cities', strict_slashes=False,
                  methods=["GET", "POST"])
 @app_views.route('/cities/<city_id>', strict_slashes=False,
                  methods=["DELETE", "GET", "PUT"])
-def get_cities(state_id=None):
+def get_cities(state_id=None, city_id=None):
     """Method to display the welcome text"""
     if state_id:
         state_obj = st.get(State, state_id)
         if state_obj:
-            if request.method == "DELETE":
-                st.delete(state_obj)
-                st.save()
-                return jsonify({}), 200
-            elif request.method == "GET":
+            if request.method == "GET":
                 my_list = []
                 for city in state_obj.cities:
                     my_list.append(city.to_dict())
                 return jsonify(my_list)
+            elif request.method == "POST":
+                data = request.get_json()
+                if not data:
+                    return jsonify(error="Not a JSON"), 400
+                elif not data.get("name"):
+                    return jsonify(error="Missing name"), 400
+                new_city = City(**data)
+                new_city.save()
+                return jsonify(new_city.to_dict()), 201
+        return abort(404)
+    elif city_id:
+        city_obj = st.get(City, city_id)
+        if city_obj:
+            if request.method == "GET":
+                return jsonify(city_obj.to_dict())
+            elif request.method == "DELETE":
+                st.delete(city_obj)
+                st.save()
+                return jsonify({}), 200
             elif request.method == "PUT":
                 data = request.get_json()
                 if not data:
                     return jsonify(error="Not a JSON"), 400
                 for key, value in data.items():
-                    if key not in ["id", "created_at", "updated_at"]:
-                        setattr(state_obj, key, value)
+                    if key not in ["id", "created_at", "updated_at",
+                                   "state_id"]:
+                        setattr(city_obj, key, value)
                         st.save()
-                return jsonify(state_obj.to_dict()), 200
+                return jsonify(city_obj.to_dict()), 200
         return abort(404)
-    else:
-        if request.method == "GET":
-            res = st.all('State').values()
-            my_list = []
-            for state in res:
-                my_list.append(state.to_dict())
-            return jsonify(my_list)
-        elif request.method == "POST":
-            data = request.get_json()
-            if not data:
-                return jsonify(error="Not a JSON"), 400
-            elif not data.get("name"):
-                return jsonify(error="Missing name"), 400
-            new_state = State(**data)
-            new_state.save()
-            return jsonify(new_state.to_dict()), 201
